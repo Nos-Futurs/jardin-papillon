f6953b07f7bbd79e0ae32a0080d34f225b30d0f9
PaulPHPE <93597135+PaulPHPE@users.noreply.github.com>
2024-07-14 20:36:45 +0200

    feat: add photos

--- src/components/ImagesContainer/ImagesContainer.tsx
+++ src/components/ImagesContainer/ImagesContainer.tsx
@@ -1,29 +1,31 @@
 import { Component, createSignal } from "solid-js";
-import pergolaJpg from "../../assets/images/pergola.jpg"
-import pergolawebp from "../../assets/images/pergola.webp"
-import serreJpg from "../../assets/images/serre.jpg"
-import serrewebp from "../../assets/images/serre.webp"
-import rencontreJpg from "../../assets/images/rencontre.jpg"
-import rencontrewebp from "../../assets/images/rencontre.webp"
+import photo_1jpg from "../../assets/images/photo_1.jpg"
+import photo_1webp from "../../assets/images/photo_1.webp"
+import fleursJpg from "../../assets/images/fleurs.jpg"
+import fleurswebp from "../../assets/images/fleurs.webp"
+import foinJpg from "../../assets/images/foin.jpg"
+import foinwebp from "../../assets/images/foin.webp"
+import plansJpg from "../../assets/images/plans.jpg"
+import planswebp from "../../assets/images/plans.webp"
 import fleche from "../../assets/fleche.svg"
 
 
 import "./ImagesContainer.scss";
 
-const images = [{ jpg: pergolaJpg, webp: pergolawebp, alt: "Pergola avec des plants de courge grimpant" }, { jpg: serreJpg, webp: serrewebp, alt: "Serre et vu sur la montagne" }, { jpg: rencontreJpg, webp: rencontrewebp, alt: "Activité de plantation de Maïs avec des enfants." }]
+const images = [{ jpg: photo_1jpg, webp: photo_1webp, alt: "" }, { jpg: fleursJpg, webp: fleurswebp, alt: "" }, { jpg: foinJpg, webp: foinwebp, alt: "" }, { jpg: plansJpg, webp: planswebp, alt: "" }]
 
 const ImagesContainer: Component = () => {
     const [number, setNumber] = createSignal<number>(0);
 
     return (
         <div id="images-container">
-            {number() !== 0 && <button onClick={() => { setNumber(number() - 1) }} class="go_back_button"><img src={fleche.src} alt="Fleche de retour à l'image précédente" class="go_back" /></button>}
+            {number() !== 0 && <button onClick={(): void => { setNumber(number() - 1) }} class="go_back_button"><img src={fleche.src} alt="Fleche de retour à l'image précédente" class="go_back" /></button>}
             <img
                 alt={images[number()].alt}
                 srcset={`${images[number()].webp.src}, ${images[number()].jpg.src}`}
                 loading="lazy"
             />
-            {number() !== images.length - 1 && <button onClick={() => { setNumber(number() + 1) }}><img src={fleche.src} alt="Fleche pour aller à l'image suivante" /></button>}
+            {number() !== images.length - 1 && <button onClick={(): void => { setNumber(number() + 1) }}><img src={fleche.src} alt="Fleche pour aller à l'image suivante" /></button>}
         </div>
     );
 };
